109d454d96c285ebf63be0c49290423d0975c3a0
akbar <syedakbarali12345@gmail.com>
2026-05-22 10:04:37 +0500

    server.js changes

--- backend/server.js
+++ backend/server.js
@@ -17,10 +17,23 @@ dotenv.config();
 
 const app = express()
 
+const allowedOrigins = [
+  "http://localhost:5173",
+  "https://task-manager-cheel.vercel.app"
+]
+
 //Middleware to handle CORS
 app.use(
   cors({
-    origin: "http://localhost:5173",
+    origin: function (origin, callback) {
+      if (!origin) return callback(null, true);
+      if (allowedOrigins.indexOf(origin) === -1) {
+        const msg = 'The CORS policy for this site does not allow access from the specified Origin.';
+        return callback(new Error(msg), false)
+      }
+
+      return callback(null, true)
+    },
     methods: ["GET", "POST", "PUT", "DELETE"],
     allowedHeaders: ["Content-Type", "Authorization"]
   })
